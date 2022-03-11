03f032241c50c1d9016e1419e843c4a5de77f805
Juanda <98669750+theYassonDC@users.noreply.github.com>
2022-03-11 11:53:38 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database:{
-        conectionLimit:10,
-        host: 'bjbwe5xnionbqe9mbe8j-mysql.services.clever-cloud.com',
-        user: 'u1zmhmkxndmzatzc',
-        password: 'jsjJTgK51hIdpG79LyMs',
-        database: 'bjbwe5xnionbqe9mbe8j'
-    }
-}
